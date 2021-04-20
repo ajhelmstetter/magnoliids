35aafa60badc00322a9c2ce48a8eac8c19c77c75
Andrew Helmstetter <andrew.j.helmstetter@gmail.com>
2021-04-20 16:54:18 +0200

    :tada: Initial commit

---
+++ make.R
@@ -0,0 +1,31 @@
+#' @title PAFTOL_magnoliids: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Andrew Helmstetter \email{andrew.j.helmstetter@gmail.com}
+#' 
+#' @date 2021/04/20
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all()
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("rscripts", "script_X.R"))

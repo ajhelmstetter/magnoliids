0616ca177057c2c1fb06a79246c5c7024a5a5ed5
ajhelmstetter <andrew.j.helmstetter@gmail.com>
2021-04-23 18:08:24 +0200

    package dependencies and exporting Rmd pdfs

--- make.R
+++ make.R
@@ -1,11 +1,11 @@
 #' @title PAFTOL_magnoliids: A Research Compendium
-#' 
-#' @description 
-#' A paragraph providing a full description of the project and describing each 
+#'
+#' @description
+#' A paragraph providing a full description of the project and describing each
 #' step of the workflow.
-#' 
+#'
 #' @author Andrew Helmstetter \email{andrew.j.helmstetter@gmail.com}
-#' 
+#'
 #' @date 2021/04/20
 
 
@@ -29,3 +29,5 @@ devtools::load_all()
 
 # List all R scripts in a sequential order and using the following form:
 # source(here::here("rscripts", "script_X.R"))
+rmarkdown::render(input = here::here("rscripts","prelim_results.Rmd"),
+                  output_file = here::here("outputs","prelim_results.pdf"))

690066dff998bdc18181265b3c2c06d62a9ab0ea
ajhelmstetter <andrew.j.helmstetter@gmail.com>
2021-05-07 12:30:48 +0200

    fixed filepaths

---
+++ data/gap_metadata/bpa-plants_Atherospermataceae_20210316T0451/README.txt
@@ -0,0 +1,49 @@
+CKAN Bulk Download
+------------------
+
+Search of organization: bpa-plants bpa-plants_Atherospermataceae_20210316T0451
+
+Bulk download package generated:
+2021-03-16T04:51:56.483856+0000
+
+This archive contains the following files:
+
+download.ps1:
+Windows PowerShell script, which when executed will download the files,
+and then checksum them. There are no dependencies other than PowerShell.
+
+download.sh:
+UNIX shell script, which when executed will download the files,
+and then checksum them. This is supported on any Linux or MacOS/BSD
+system, so long as `curl` is installed.
+
+Before running either of these scripts, please set the CKAN_API_KEY
+environment variable.
+
+You can find your API Key by browsing to:
+https://data.bioplatforms.com/user/hervesauquet
+
+The API key has the format:
+xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+To set the environment variable in Linux/MacOS/Unix, use:
+export CKAN_API_KEY=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+On Microsoft Windows, within Powershell, use:
+$env:CKAN_API_KEY=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+package_metadata folder:
+Contains metadata spreadsheets for all selected data packages, grouped by
+the type of package (schema). Each data package will contain one or more
+resources. This metadata is an amalgamation of all metadata, including
+sample contextual metadata and processing metadata.
+
+resource_metadata folder:
+Contains metadata spreadsheets for all selected data resources (files).
+
+QUERY.txt:
+Text file which contains metadata about the download results and the original
+query
+
+tmp folder:
+This folder contains files required by the download scripts. Its
+contents can be ignored.
---
+++ data/gap_metadata/bpa-plants_Myristicaceae_20210316T0453/download.ps1
@@ -0,0 +1,75 @@
+#!/usr/bin/env pwsh
+
+
+$apikey = $Env:CKAN_API_KEY
+if (!$apikey) {
+  'Please set the CKAN_API_KEY environment variable.'
+  ''
+  'You can find your API Key by browsing to:'
+  'https://data.bioplatforms.com/user/hervesauquet'
+  ''
+  'The API key has the format:'
+  'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+  ''
+  'To set the environment variable in Linux/MacOS/Unix, use:'
+  'export CKAN_API_KEY=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+  ''
+  'On Microsoft Windows, within Powershell, use:'
+  '$env:CKAN_API_KEY=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+  exit 1
+}
+
+
+#
+# This PowerShell script was automatically generated.
+#
+
+function DownloadURL($url)
+{
+    $filename = $url.Substring($url.lastIndexOf('/') + 1)
+    if (Test-Path $filename) {
+        "File already exists, skipping download: " + $filename
+        return
+    }
+    $client = new-object System.Net.WebClient
+    if ($apikey) {
+        $client.Headers.Add('Authorization: ' + $apikey)
+    }
+    "Downloading: " + $filename
+    $client.DownloadFile($url, $filename)
+}
+
+function VerifyMD5([String]$filename, [String]$expected_md5)
+{
+    $md5hash = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+    try {
+        $actual_md5 = [System.BitConverter]::ToString($md5hash.ComputeHash([System.IO.File]::ReadAllBytes($filename))).Replace('-', '').toLower();
+    } catch [System.IO.FileNotFoundException] {
+        $filename + ": FAILED open or read"
+        return
+    }
+    if ($actual_md5 -eq $expected_md5) {
+        $filename + ": OK"
+    } else {
+        $filename + ": FAILED"
+    }
+}
+
+
+'Commencing bulk download of data from CKAN:'
+''
+
+$urls = Get-Content 'tmp/bpa-plants_Myristicaceae_20210316T0453_urls.txt'
+ForEach ($line in $urls) {
+    DownloadURL $line
+}
+
+'File downloads complete.'
+''
+'Verifying file checksums:'
+''
+$md5s = Get-Content 'tmp/bpa-plants_Myristicaceae_20210316T0453_md5sum.txt'
+ForEach ($line in $md5s) {
+    $md5, $filename = $line.Split(" ",[StringSplitOptions]'RemoveEmptyEntries')
+    VerifyMD5 $filename $md5
+}
